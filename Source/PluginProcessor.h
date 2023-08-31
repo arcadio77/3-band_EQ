422b33f9843b74c16e1ce882cbee8ab50d886357
Arcadio Morales <arkadiuszsieradzki96@gmail.com>
2023-08-31 17:51:04 +0200

    DSP config

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -24,7 +24,7 @@ public:
     ~_3band_EQAudioProcessor() override;
 
     //==============================================================================
-    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
     void releaseResources() override;
 
    #ifndef JucePlugin_PreferredChannelConfigurations
@@ -61,6 +61,12 @@ public:
     juce::AudioProcessorValueTreeState apvts{*this, nullptr, "Parameters", createParameterLayout()};
 
 private:
+    using Filter = juce::dsp::IIR::Filter<float>;
+    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+
+    MonoChain leftChain, rightChain;
+
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (_3band_EQAudioProcessor)
 };
